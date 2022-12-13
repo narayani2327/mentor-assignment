d593cb9c36d0a1c8b6476ad8c0ddb4a4b9bad58f
narayani2327 <harikalanarayani@gmail.com>
2022-12-13 21:45:12 +0530

    get data from files

--- main-code.py
+++ main-code.py
@@ -7,11 +7,11 @@ class Mentor:
         self.experience=experience
         self.subject=subject
     def display(self):
-        print("Mentor details")
-        print("Name",self.name)
-        print("Subject",self.subject)
-        print("Department",self.department)
-        print("Experience",self.experience)
+        print("\nName:",self.name,end="")
+        print("Subject:",self.subject,end="")
+        print("Department:",self.department,end="")
+        print("Experience:",self.experience,end="")
+
 class Student:
     def __init__(self,name, numberOfSubjects, marks,usn,year):
         self.name=name
@@ -20,13 +20,14 @@ class Student:
         self.usn=usn
         self.year=year
     def display(self):
-        print("Student details")
-        print("Name",self.name)
-        print("USN",self.usn)
-        print("Year",self.year)
-        print("Number of subjects",self.numberOfSubjects)
+        print("\n\n---------Student details---------")
+        print("Name:",self.name,end="")
+        print("USN:",self.usn,end="")
+        print("Year:",self.year,end="")
+        print("Number of subjects:",self.numberOfSubjects,end="")
         for i in self.marks:
             i.display()
+
 class Subject:
     def __init__(self, name, marks, subjectID, credit):
         self.name=name
@@ -34,45 +35,46 @@ class Subject:
         self.credit=credit
         self.mark=marks
     def display(self):
-        print("Subject details")
-        print("Name",self.name)
-        print("Subject ID",self.subjectID)
-        print("Credits",self.credit)
-        print("Marks",self.mark)
+        print("\nSubject details")
+        print("Name:",self.name,end="")
+        print("Subject ID:",self.subjectID,end="")
+        print("Credits:",self.credit,end="")
+        print("Marks:",self.mark,end="")
     
 print("-----------Welcome to Mentor assignment program-----------")
-numberOfStudents=int(input("Please enter the number of students -- "))
-numberOfMentors=int(input("Please enter the number of mentors -- "))
 mentors=[]
 students=[]
-print("Please enter mentor details")
-for i in range(numberOfMentors):
-    print("\nMentor",i+1, "details")
-    name=input("Enter name ")
-    department=input("Enter department ")
-    experience=input("Enter experience ")
-    subject=input("Enter subject ")
-    m = Mentor(name, department, experience, subject)
-    mentors.append(m)
+# print("\nMentor details")
+# with open("mentor-data.txt") as fp:
+#     while True:
+#         name=fp.readline()
+#         department=fp.readline()
+#         experience=fp.readline()
+#         subject=fp.readline()
+#         if not name:
+#             break
+#         m = Mentor(name, department, experience, subject)
+#         m.display()    
+#         mentors.append(m)
 
-print("Please enter students details")
-for i in range(numberOfStudents):
-    print("\nStudent",i+1, "details")
-    name=input("Enter name ")
-    usn=input("Enter usn ")
-    year=input("Enter year ")
-    numberOfSubjects=int(input("Enter number of subjects "))
-    sub=[]
-    for j in range(numberOfSubjects):
-        print("Subject",j+1, "details")
-        subjectName=input("Enter subject name ")
-        subjectID=input("Enter subjectID ")
-        credit=input("Enter credits ")
-        marks=input("Enter marks ")
-        s= Subject(subjectName, marks, subjectID, credit)
-        sub.append(s)
-    st = Student(name, numberOfSubjects, sub, usn, year)
-    students.append(st)
+# print("Please enter students details")
+# for i in range(numberOfStudents):
+#     print("\nStudent",i+1, "details")
+#     name=input("Enter name ")
+#     usn=input("Enter usn ")
+#     year=input("Enter year ")
+#     numberOfSubjects=int(input("Enter number of subjects "))
+#     sub=[]
+#     for j in range(numberOfSubjects):
+#         print("Subject",j+1, "details")
+#         subjectName=input("Enter subject name ")
+#         subjectID=input("Enter subjectID ")
+#         credit=input("Enter credits ")
+#         marks=input("Enter marks ")
+#         s= Subject(subjectName, marks, subjectID, credit)
+#         sub.append(s)
+#     st = Student(name, numberOfSubjects, sub, usn, year)
+#     students.append(st)
 
 
 # for i in students:
@@ -82,14 +84,35 @@ for i in range(numberOfStudents):
 
 
 
-for i in students:
-    max_attr = min(i.marks, key=attrgetter('mark'))
-    print(max_attr.mark, max_attr.name)
-    mento=[]
-    for j in mentors:
-        if j.subject==max_attr.name:
-            mento.append(j)
-    if len(mento)>1:
-        print(max(mento.experience))
-    else:
-        print(mento[0].name)
+# for i in students:
+#     max_attr = min(i.marks, key=attrgetter('mark'))
+#     print(max_attr.mark, max_attr.name)
+#     mento=[]
+#     for j in mentors:
+#         if j.subject==max_attr.name:
+#             mento.append(j)
+#     if len(mento)>1:
+#         print(max(mento.experience))
+#     else:
+#         print(mento[0].name)
+
+
+with open("student-data.txt") as fp:
+    while True:
+        name=fp.readline()
+        usn=fp.readline()
+        year=fp.readline()
+        numberOfSubjects=fp.readline()
+        if not name:
+            break
+        sub=[]
+        for j in range(int(numberOfSubjects)):
+            subjectName=fp.readline()
+            subjectID=fp.readline()
+            credit=fp.readline()
+            marks=fp.readline()
+            s= Subject(subjectName, marks, subjectID, credit)
+            sub.append(s)
+        st = Student(name, numberOfSubjects, sub, usn, year)
+        students.append(st)
+        st.display()
